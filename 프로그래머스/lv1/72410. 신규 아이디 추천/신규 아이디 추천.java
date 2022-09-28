aee7c737ed2b7811e5a82128b0cd35308e98266c
dksgksrlf <49630393+dksgksrlf@users.noreply.github.com>
2022-09-28 20:30:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 5.06 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,47 +1,6 @@
 class Solution {
     public String solution(String new_id) {
-        new_id=new_id.toLowerCase();
-        String[] id=new_id.split("");
-        String answer ="";
-       
-        for(String token:id){
-            
-            if(token.charAt(0)>='a'&&token.charAt(0)<='z'){
-                
-            }else if(token.charAt(0)>='0'&&token.charAt(0)<='9'){
-                
-            }else if(token.compareTo("-")==0){
-                
-            }else if(token.compareTo("_")==0){
-                
-            }else if(token.compareTo(".")==0){
-                
-            }else{
-                token="";
-            }
-            answer+=token;
-        }
-        while(answer.indexOf("..")!=-1)
-            answer=answer.replace("..",".");
-        if(answer.charAt(0)=='.'){
-            answer=answer.substring(1);
-        }
-        if(answer.length()!=0&&answer.charAt(answer.length()-1)=='.'){
-            answer=answer.substring(0,answer.length()-1);
-        }
-        if(answer.equals("")){
-            answer="a";
-        }
-        if(answer.length()>=16){
-            answer=answer.substring(0,15);
-            if(answer.charAt(answer.length()-1)=='.'){
-                answer=answer.substring(0,answer.length()-1);
-            }
-        }
-        while(answer.length()<3){
-            answer+=answer.substring(answer.length()-1);
-        }
-       
+        String answer = "";
         return answer;
     }
 }
